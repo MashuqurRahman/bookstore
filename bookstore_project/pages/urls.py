42f162c36b3604b748bd05b8cac33051fd21822c
mashuqur rahman <mashuq.tanmny06@gmail.com>
2020-07-27 21:36:51 +0600

    static assets ch7

---
+++ bookstore_project/pages/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import HomePageView,AboutPageView
+
+urlpatterns = [
+   
+    path('',HomePageView,name="home"),
+    path('about',AboutPageView,name="about")
+]
---
+++ bookstore_project/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def HomePageView(request):
+    return render(request,'home.html')
+
+def AboutPageView(request):
+    return render(request,'about.html')
